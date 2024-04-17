03e198ff5193c066cbaf742b09e5f1f478ae0c99
Widhori <141535537+Widhori@users.noreply.github.com>
2024-04-17 21:16:40 +0800

    简单模拟实现循环数组队列

--- src/com/atcaomin/queue/CircleArrayQueueDemo.java
+++ src/com/atcaomin/queue/CircleArrayQueueDemo.java
@@ -1,13 +1,11 @@
 package com.atcaomin.queue;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayQueueDemo {
+public class CircleArrayQueueDemo {
     public static void main(String[] args) {
-        //测试一下队列
-        //创建一个队列
-        ArrayQueue arrayQueue = new ArrayQueue(3);
+        //测试模拟环形队列是否正确
+        CircleArray circleArray = new CircleArray(4);
         char key = ' ';
         Scanner scanner = new Scanner(System.in);
         boolean loop = true;
@@ -20,16 +18,16 @@ public class ArrayQueueDemo {
             key = scanner.next().charAt(0);//接收一个字符
             switch (key) {
                 case 's':
-                    arrayQueue.showQueue();
+                    circleArray.showQueue();
                     break;
                 case 'a':
                     System.out.println("输入一个数:");
                     int value = scanner.nextInt();
-                    arrayQueue.addQueue(value);
+                    circleArray.addQueue(value);
                     break;
                 case 'g':
                     try {
-                        int res = arrayQueue.getQueue();
+                        int res = circleArray.getQueue();
                         System.out.println(res);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
@@ -37,7 +35,7 @@ public class ArrayQueueDemo {
                     break;
                 case 'h':
                     try {
-                        int res = arrayQueue.headQueue();
+                        int res = circleArray.headQueue();
                         System.out.println(res);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
@@ -55,24 +53,22 @@ public class ArrayQueueDemo {
     }
 }
 
-//使用数组模拟实现队列-编写一个ArrayQueue类
-class ArrayQueue {
+class CircleArray {
     private int maxSize;//队列最大容量
-    private int rear;//队列的最后一个元素
-    private int front;//对头的前一个位置
+    private int rear;//队列最后一个元素的后一个位置
+    private int front;//指向队列的第一个元素
     private int[] array;//用于存放数据，模拟队列
 
     //创建构造方法
-    public ArrayQueue(int arrMaxSize) {
+    public CircleArray(int arrMaxSize) {
         maxSize = arrMaxSize;
         array = new int[maxSize];
-        rear = -1;
-        front = -1;
+        rear = 0;
+        front = 0;
     }
-
     //判断队列是否为满
     public boolean isFull() {
-        return rear == maxSize;
+        return (rear + 1) % maxSize == front;
     }
 
     //判断队列是否为空
@@ -85,19 +81,20 @@ class ArrayQueue {
         if(isFull()) {
             System.out.println("队列已满，无法添加");
         }else {
-            rear++;
             array[rear] = n;
+            rear = (rear + 1) % maxSize;
         }
     }
 
     //获取数据——出队列
     public int getQueue() {
-       if(isEmpty()) {
-           throw new RuntimeException("队列为空，无法获取数据");
-       }else {
-           front++;
-           return array[front];
-       }
+        if(isEmpty()) {
+            throw new RuntimeException("队列为空，无法获取数据");
+        }else {
+            int value = array[front];
+            front = (front + 1) % maxSize;
+            return value;
+        }
     }
 
     //显示队列
@@ -105,10 +102,15 @@ class ArrayQueue {
         if(isEmpty()) {
             System.out.println("队列为空，无法显示");
         }else {
-            for (int i = front + 1; i <= rear; i++) {
-                System.out.printf("array[%d] = %d\n",i,array[i]);
+            for (int i = front; i < front + size(); i++) {
+                System.out.printf("array[%d] = %d\n",i % maxSize,array[i % maxSize]);
             }
         }
+    }
+
+    //获取队列元素个数
+    public int size() {
+        return (rear + maxSize - front) % maxSize;
     }
 
     //显示对列的头数据是多少，注意这不是去除数据
@@ -116,7 +118,8 @@ class ArrayQueue {
         if(isEmpty()) {
             throw new RuntimeException("队列为空");
         }else {
-            return array[front + 1];
+            return array[front];
         }
     }
+
 }
