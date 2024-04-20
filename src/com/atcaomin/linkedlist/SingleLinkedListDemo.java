9ad6cabf44e5c6963a1ac718c0a6158c99115b43
Widhori <141535537+Widhori@users.noreply.github.com>
2024-04-20 16:44:53 +0800

    模拟实现了单链表并完成了几道面试题

--- src/com/atcaomin/linkedlist/SingleLinkedListDemo.java
+++ src/com/atcaomin/linkedlist/SingleLinkedListDemo.java
@@ -1,5 +1,7 @@
 package com.atcaomin.linkedlist;
 
+import java.util.Stack;
+
 public class SingleLinkedListDemo {
 
     public static void main(String[] args) {
@@ -24,6 +26,24 @@ public class SingleLinkedListDemo {
         singleLinkedList.addByOrder(hero3);
         singleLinkedList.addByOrder(hero2);
         singleLinkedList.showList();
+        System.out.println();
+        //singleLinkedList.reverse();
+        //singleLinkedList.reversePrint();
+        //singleLinkedList.showList();
+        singleLinkedList.print(singleLinkedList.getHead().next);
+
+
+        //测试修改节点的代码
+        /*HeroNode newHeroNode = new HeroNode(2,"小卢","玉麒麟");
+        singleLinkedList.update(newHeroNode);
+        singleLinkedList.showList();*/
+
+        //测试删除的代码
+        /*singleLinkedList.delete(3);
+        singleLinkedList.showList();*/
+
+        //测试单链表的有效节点个数
+        //System.out.println(singleLinkedList.getLength());
     }
 
 }
@@ -46,6 +66,11 @@ class SingleLinkedList {
             }
             cur = cur.next;
         }
+    }
+
+    //获取头节点
+    public HeroNode getHead() {
+        return head;
     }
 
     //添加节点到链表
@@ -75,6 +100,55 @@ class SingleLinkedList {
         heroNode.next = cur.next;
         cur.next = heroNode;
     }
+    //修改链表的信息，即根据id来修改
+    public void update(HeroNode newHeroNode) {
+        HeroNode cur = head;
+        boolean flag = false;
+        while (true) {
+            //先判断是否为空
+            if(isEmpty()) {
+                System.out.println("链表为空");
+            }
+            //没有找到对应的编号id
+            if(cur == null) {
+                break;
+            }
+            if(cur.id == newHeroNode.id) {
+                flag = true;
+                break;
+            }
+            cur = cur.next;
+        }
+        //根据flag判断是否找到要修改的节点
+        if(flag) {
+            cur.name = newHeroNode.name;
+            cur.nickName = newHeroNode.nickName;
+        }else {
+            System.out.printf("没有找到编号为%d的节点，无法修改",newHeroNode.id);
+        }
+    }
+
+    //删除节点
+    public void delete(int id) {
+        HeroNode cur = head;
+        boolean flag = false;
+        while (true) {
+            if (cur.next == null) {//已经到链表最后了
+                break;
+            }
+            if(cur.next.id == id) {//找到待删除的前一个节点cur
+                flag = true;
+                break;
+            }
+            cur = cur.next;
+        }
+        if(flag) {
+            cur.next = cur.next.next;
+        }else {
+            System.out.printf("要删除的节点%d不存在",id);
+        }
+    }
+
     //显示链表
     public void showList() {
         HeroNode cur = head.next;
@@ -85,6 +159,64 @@ class SingleLinkedList {
             System.out.println(cur);
             cur = cur.next;
         }
+    }
+
+    //获取单链表的节点个数（如果是带头节点的链表，不统计头节点）
+    public int getLength() {
+        if(head.next == null) {
+            return 0;
+        }
+        int length = 0;
+        HeroNode cur = head.next;
+        while (cur != null){
+            length++;
+            cur = cur.next;
+        }
+        return length;
+    }
+
+    //翻转链表
+    public void reverse() {
+        if(head.next == null || head.next.next == null) {//链表为空或者只有一个节点
+            return;
+        }
+        HeroNode reverseHead = new HeroNode(0,null,null);
+        HeroNode cur = head.next;
+        HeroNode next = null;
+        while(cur != null) {
+            next = cur.next;
+            cur.next = reverseHead.next;
+            reverseHead.next = cur;
+            cur = next;
+        }
+        head.next = reverseHead.next;
+    }
+
+    //判断链表是否为空
+    public boolean isEmpty() {
+        return head.next == null;
+    }
+
+    //逆序打印链表
+    //使用栈
+    public void reversePrint() {
+        Stack<HeroNode> stack = new Stack<>();
+        HeroNode cur = head.next;
+        while (cur != null) {
+            stack.push(cur);
+            cur =cur.next;
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+    //使用递归
+    public void print(HeroNode cur) {
+        if(cur == null) {
+            return;
+        }
+        print(cur.next);
+        System.out.println(cur);
     }
 }
 
